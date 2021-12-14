5c44b2581aebd1fe0c66506abacccce26e982e7b
Thomas Lin <thomas.lin@spaceryde.com>
2021-12-14 10:25:24 -0500

    Updated index.js to print context info

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const core = require('@actions/core');
 const wait = require('./wait');
-
+const github = require('@actions/github');
 
 // most @actions toolkit packages have async methods
 async function run() {
+
+  // tlin's shit
+  //console.log(github.context)
+  core.info(JSON.stringify(github.context))
+
   try {
     const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    core.info(`Tlin's shit is waiting ${ms} milliseconds ...`);
 
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
